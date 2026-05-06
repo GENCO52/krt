c2d198cf6367c275cc696307f7a8eef7301e5bce
Şükrü Gençoğlu <sukrugencoglu10@gmail.com>
2026-05-06 17:23:15 +0300

    Supabase URL düzeltildi (Storage yolu hatası giderildi)

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 // Supabase config
-const SUPABASE_URL = 'https://kmuvwztjwetjfwvgcmyf.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://kmuvwztjwetjfwvgcmyf.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttdXZ3enRqd2V0amZ3dmdjbXlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNjAxNDMsImV4cCI6MjA5MzYzNjE0M30.XnTxAiot4sTlK1_tlz0SidQRdThT8R2vS5jZkRfUSog';
 
 // Initialize Supabase Client
